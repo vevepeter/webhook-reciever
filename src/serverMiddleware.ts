b55ff0277afae4a54a344fa588c13853657bb4d7
mephest0 <peter@vev.design>
2023-11-24 12:57:46 +0100

    Added a ToC as fallback for requests to `/`

--- src/serverMiddleware.ts
+++ src/serverMiddleware.ts
@@ -5,7 +5,22 @@ import { sanitizePath } from './helpers'
 export function serverMiddleware(req: Request, res: Response, next: NextFunction) {
   const path = sanitizePath(req.path)
 
-  if (files[path]) return res.send(files[path])
+  if (files[path])
+    return res.send(files[path])
+
+  // if nothing has been published to `/`, serve a table of contents
+  if (path === '')
+    return res.send(getList())
 
   next()
 }
+
+/**
+ * Generates a list of all files on the "server", with links
+ */
+const getList = () => Object.keys(files).length ?
+  Object
+    .keys(files)
+    .map(path => `<a href="${path}">${path}</a>`)
+    .join('<br />') :
+  '<h1>Nothing has been published yet</h1>'
