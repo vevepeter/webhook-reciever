88ea06f6f628fee3de2891f4182e57f4fb11a2d4
mephest0 <peter@vev.design>
2023-11-24 12:59:32 +0100

    Changed order of logged request data

--- src/loggerMiddleware.ts
+++ src/loggerMiddleware.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express'
 export function loggerMiddleware(req: Request, _res: Response, next: NextFunction) {
   const length = (JSON.stringify(req.body) || '').length
 
-  console.log(`[${req.method}]\t`, req.path, '\t', length + 'bytes')
+  console.log(`[${req.method}]`, '\t', `${length}bytes`, '\t', req.path)
 
   next()
 }
