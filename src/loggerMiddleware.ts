ed6a5ba45ece518445904c76b0fdad9f26eb7069
mephest0 <peter@vev.design>
2023-11-28 11:31:07 +0100

    Log IP address in logger middleware

--- src/loggerMiddleware.ts
+++ src/loggerMiddleware.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express'
 export function loggerMiddleware(req: Request, _res: Response, next: NextFunction) {
   const length = (JSON.stringify(req.body) || '').length
 
-  console.log(`[${req.method}]`, '\t', `${length}bytes`, '\t', req.path)
+  console.log(`[${req.method}]`, '\t', req.ip, '\t', `${length}bytes`, '\t', req.path)
 
   next()
 }
